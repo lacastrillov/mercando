83e8e082be1c331930d62f1287789c980907c578
Luis Castrillo <lcastrillo@grupodot.com>
2018-03-07 18:02:13 -0500

    New External Service Layer Fase2

--- mercando-rest/src/main/java/com/lacv/mercando/controllers/rest/process/ExternalServiceProcessController.java
+++ mercando-rest/src/main/java/com/lacv/mercando/controllers/rest/process/ExternalServiceProcessController.java
@@ -12,15 +12,9 @@ import com.lacv.mercando.model.entities.User;
 import com.lacv.mercando.services.LogProcessService;
 import com.lacv.mercando.services.security.SecurityService;
 import com.dot.gcpbasedot.controller.RestProcessController;
-import com.dot.gcpbasedot.dto.ExternalServiceDto;
-import com.lacv.mercando.model.dtos.process.BasicPDto;
-import com.lacv.mercando.model.dtos.process.NetworkPDto;
-import com.lacv.mercando.model.dtos.process.ProductoPDto;
-import com.lacv.mercando.model.dtos.process.SolicitudePDto;
-import com.lacv.mercando.model.dtos.process.UsuarioPDto;
+import com.lacv.mercando.services.external.NovaventaService;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,31 +32,15 @@ public class ExternalServiceProcessController extends RestProcessController {
     @Autowired
     SecurityService securityService;
     
+    @Autowired
+    NovaventaService novaventaService;
+    
     
     @PostConstruct
     public void init(){
         super.addControlProcess("externalService", LogProcess.class, logProcessService);
         
-        ExternalServiceDto service0= new ExternalServiceDto("maquinasNovaventaXml", "https://portal-contenido-novaventa.appspot.com/rest/{entity}/find/xml.htm", HttpMethod.GET, SolicitudePDto.class);
-        service0.setResponseDataFormat(ExternalServiceDto.XML);
-        super.enableExternalService(service0);
-        
-        ExternalServiceDto service1= new ExternalServiceDto("maquinasNovaventa", "https://portal-contenido-novaventa.appspot.com/rest/{entity}/find.htm", HttpMethod.GET, SolicitudePDto.class);
-        super.enableExternalService(service1);
-        
-        ExternalServiceDto service2= new ExternalServiceDto("merakiDevices", "https://dashboard.meraki.com/api/v0/networks/{networkId}/devices", HttpMethod.GET, NetworkPDto.class);
-        super.enableExternalService(service2);
-        
-        ExternalServiceDto service3= new ExternalServiceDto("noticiasCarroya", "http://www.carroya.com/noticias/", HttpMethod.POST, BasicPDto.class);
-        service3.setResponseDataFormat(ExternalServiceDto.HTML);
-        super.enableExternalService(service3);
-        
-        ExternalServiceDto service4= new ExternalServiceDto("estaInBody", "http://localhost:8084/tempprocess/ajax/inbody", HttpMethod.POST, UsuarioPDto.class);
-        service4.setModeSendingData(ExternalServiceDto.IN_BODY);
-        super.enableExternalService(service4);
-        
-        ExternalServiceDto service5= new ExternalServiceDto("estaInParameters", "http://localhost:8084/tempprocess/ajax/inparameters", HttpMethod.POST, ProductoPDto.class);
-        super.enableExternalService(service5);
+        super.enableExternalService(novaventaService);
     }
     
     @Override
--- mercando-rest/src/main/java/com/lacv/mercando/controllers/rest/process/SoapClientsProcessController.java
+++ mercando-rest/src/main/java/com/lacv/mercando/controllers/rest/process/SoapClientsProcessController.java
@@ -12,10 +12,7 @@ import com.lacv.mercando.model.entities.User;
 import com.lacv.mercando.services.LogProcessService;
 import com.lacv.mercando.services.security.SecurityService;
 import com.dot.gcpbasedot.controller.RestProcessController;
-import com.dot.gcpbasedot.dto.SOAPServiceDto;
-import com.lacv.mercando.model.dtos.process.BasicPDto;
-import com.lacv.mercando.model.dtos.process.PlayerUserPDto;
-import com.lacv.mercando.model.dtos.process.UserNamePDto;
+import com.lacv.mercando.services.external.BackbaseTrainingService;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -35,22 +32,15 @@ public class SoapClientsProcessController extends RestProcessController {
     @Autowired
     SecurityService securityService;
     
+    @Autowired
+    BackbaseTrainingService backbaseTrainingService;
+    
     
     @PostConstruct
     public void init(){
         super.addControlProcess("soapClients", LogProcess.class, logProcessService);
         
-        SOAPServiceDto service1= new SOAPServiceDto("getBestScore","http://0.0.0.0:9999/training-server/PlayerManager", UserNamePDto.class);
-        super.enableSOAPClient(service1);
-        
-        SOAPServiceDto service2= new SOAPServiceDto("getPlayer","http://0.0.0.0:9999/training-server/PlayerManager", UserNamePDto.class);
-        super.enableSOAPClient(service2);
-        
-        SOAPServiceDto service3= new SOAPServiceDto("getAllPlayers","http://0.0.0.0:9999/training-server/PlayerManager", BasicPDto.class);
-        super.enableSOAPClient(service3);
-        
-        SOAPServiceDto service4= new SOAPServiceDto("createPlayer","http://0.0.0.0:9999/training-server/PlayerManager", PlayerUserPDto.class);
-        super.enableSOAPClient(service4);
+        super.enableExternalService(backbaseTrainingService);
     }
     
     @Override
